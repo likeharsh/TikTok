528aab670b3267b1502632809823be78c26cd6c1
likeharsh <21051651@kiit.ac.in>
2024-09-29 20:03:12 +0530

    initial commit1

---
+++ tiktok/src/Video.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import "./Video.css";
+
+function Video() {
+  return (
+    <div className='video'>
+        <video src='C:\Users\Harsh_1651\Desktop\tik tok videos\My Top 5 Rarest Rubik’s cubes #shorts.mp4'></video>
+    </div>
+  );
+}
+
+export default Video;
